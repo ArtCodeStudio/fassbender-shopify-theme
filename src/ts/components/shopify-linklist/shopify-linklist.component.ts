40a29bac0ab966b81f181b784a7c08d7475b41a5
JumpLink <pascal@jumplink.eu>
2019-10-24 14:16:25 +0200

    Added lookbook pages

--- src/ts/components/shopify-linklist/shopify-linklist.component.ts
+++ src/ts/components/shopify-linklist/shopify-linklist.component.ts
@@ -1,6 +1,35 @@
 import { Component, Debug } from '@ribajs/core';
 import template from './shopify-linklist.component.html';
 
+export interface LinklistLink {
+  active: boolean;
+  child_active: boolean;
+  handle: string;
+  level: number;
+  levels: number;
+  links: LinklistLink[];
+  title: string;
+  type: string;
+  url: string;
+
+  // custom
+  collapseable?: boolean;
+  collapsed?: boolean;
+}
+
+export interface Linklist {
+  handle: string;
+  id: string | null;
+  levels: number;
+  links: LinklistLink[];
+  title: string;
+}
+
+export interface Scope {
+  linklist?: Linklist;
+  toggle: ShopifyLinklistComponent['toggle'];
+}
+
 /**
  * shopify-filter
  */
@@ -14,11 +43,18 @@ export class ShopifyLinklistComponent extends Component {
     return ['name', 'linklist', 'pills', 'vertical'];
   }
 
-  protected scope: any = {};
+  protected scope: Scope = {
+    toggle: this.toggle,
+  };
 
   constructor(element?: HTMLElement) {
     super(element);
     this.init(ShopifyLinklistComponent.observedAttributes);
+  }
+
+  public toggle(link: LinklistLink) {
+    this.debug('toggle', link);
+    link.collapsed = !link.collapsed;
   }
 
   public attributeChangedCallback(name: string, oldValue: any, newValue: any, namespace: string | null) {
@@ -28,6 +64,26 @@ export class ShopifyLinklistComponent extends Component {
     // set linklist by name
     if (name === 'name') {
       this.scope.linklist = window.model.system.linklists[newValue];
+    }
+  }
+
+  protected async beforeBind(): Promise<any> {
+    super.beforeBind();
+    this.transformLinklist();
+  }
+
+  protected transformLinklist() {
+    this.debug('current linklist', this.scope.linklist);
+    if (this.scope.linklist) {
+      for (const link of this.scope.linklist.links) {
+        if (link.url === '#collapse') {
+          link.collapseable = true;
+          link.collapsed = true;
+        } else {
+          link.collapseable = false;
+          link.collapsed = true;
+        }
+      }
     }
   }
 
